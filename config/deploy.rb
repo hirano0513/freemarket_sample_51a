65d849b285018cb6ce8c210c3afafc1cfa1957ef
YamamotoMao35 <nokiroram@gmail.com>
2019-05-27 13:01:30 +0900

    Update deploy.rb
    Update deploy.rb
    
    database.ymlは非表示ファイルではないため記述を削除した。

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ set :default_env, {
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
