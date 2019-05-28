7d8790493ad8293b9598a3025acdf604a89b55d9
hirano0513 <shimaimo@gmail.com>
2019-05-28 12:05:15 +0900

    Update application_controller.rb
    devise用の設定追加
    before_action :configure_permitted_parameters
    devise_parameter_sanitizer.permit
    でnickname追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :authenticate_user! # 未ログイン時はログインページに遷移
+  before_action :configure_permitted_parameters, if: :devise_controller? # deviseでのサインアップ時にカラムを追加する
 
-  private
+    private
 
-  def production?
-    Rails.env.production?
-  end
+      def production?
+        Rails.env.production?
+      end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+      def basic_auth
+        authenticate_or_request_with_http_basic do |username, password|
+          username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+        end
+      end
+
+    protected
+
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+      end
 end
