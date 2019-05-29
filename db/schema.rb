0023b2cb15306c0e76f2a275f8a3d28f4948176e
hirano0513 <shimaimo@gmail.com>
2019-05-29 17:35:53 +0900

    migration_file&schema_file
    SNS認証のため編集
    usersテーブルにuidカラムとproviderカラムを追加したが
    後に不要となったため削除しました。
    
    代わりにsns_sns_credentialsテーブルを作成。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_081810) do
+ActiveRecord::Schema.define(version: 2019_05_29_042525) do
+
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_05_27_081810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sns_credentials", "users"
 end
