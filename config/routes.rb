ff19e52dbe93ca4415757d912459769475d0e193
YamamotoMao35 <nokiroram@gmail.com>
2019-06-05 15:24:54 +0900

    Update routes.rb
    Update routes.rb
    
    ページ移動に関するルーティングを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,21 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get 'users/sign_up/index', to: 'users/registrations#index'
-    get 'log_out', to: 'users/sessions#log_out'
+    get 'users/log_out', to: 'users/sessions#log_out'
     get 'users/sms_auth', to: 'users/registrations#sms_auth'
     get 'users/address', to: 'users/registrations#address'
     get 'users/payment', to: 'users/registrations#payment'
     get 'users/registered', to: 'users/registrations#registered'
   end
 
-  resources :items, only: [:index, :new]
+  resources :credit_cards, only: [:create, :edit]
 
-  resources :personals, only: [:show]
+  resources :users, only: [:show, :edit, :update] do
+    resources :personals, only: [:show]
+  end
+
+  resources :items, only: [:index, :new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'items#index'
 end
