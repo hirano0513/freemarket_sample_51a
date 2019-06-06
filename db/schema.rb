38ed5a51c5e50ccb106f5c51020eea5c3f50c8ea
YamamotoMao35 <nokiroram@gmail.com>
2019-06-06 17:04:41 +0900

    Create buyer_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_080203) do
+ActiveRecord::Schema.define(version: 2019_06_06_080341) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_080203) do
     t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "buyer_id"
     t.index ["name"], name: "index_items_on_name"
   end
 
