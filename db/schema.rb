2452d3930da80f52003792e99670a76504da155b
YamamotoMao35 <nokiroram@gmail.com>
2019-06-06 16:58:42 +0900

    Revert "Change column buyer_id"
    This reverts commit 4cad6bbd4dd96feb5483320550e231d102873496.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_074900) do
+ActiveRecord::Schema.define(version: 2019_06_06_072214) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
