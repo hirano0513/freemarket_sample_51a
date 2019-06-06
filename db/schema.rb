f103347ed6331cad4fa5f982b4851d1874cf7b66
YamamotoMao35 <nokiroram@gmail.com>
2019-06-06 17:02:57 +0900

    Remove buyer_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_072214) do
+ActiveRecord::Schema.define(version: 2019_06_06_080203) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_06_06_072214) do
     t.integer "sales_status", default: 0, null: false
     t.integer "like"
     t.integer "seller_id", null: false
-    t.integer "buyer_id", null: false
     t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
