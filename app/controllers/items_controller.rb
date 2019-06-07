971851048281b5bdb865a1207b724866e1c1a928
YamamotoMao35 <nokiroram@gmail.com>
2019-06-07 13:53:23 +0900

    Update items_controller.rb
    Update items_controller.rb
    
    コントローラーにnew、createアクションを定義した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,27 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all.order("id DESC").limit(4)
   end
 
   def new
+    @item = Item.new
+    @shipment = Shipment.new
   end
+
+  def create
+    @item = Item.create(item_params)
+    @shipment = Shipment.create(shipment_params)
+    redirect_to root_path
+
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :size, :price, :item_status)
+  end
+
+  def shipment_params
+    params.require(:shipment).permit(:cost_payer, :method, :days, :prefecture_id)
+  end
+
 end
