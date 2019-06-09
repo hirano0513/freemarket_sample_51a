bc0f456ed42c61d1e23647e5df089baad6423522
YamamotoMao35 <nokiroram@gmail.com>
2019-06-09 14:22:33 +0900

    Update item.rb
    Update item.rb
    
    アソシエーションを変更した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,17 @@ class Item < ApplicationRecord
   validates :description, length: {maximum: 1000}
   validates :price, numericality: {only_integer: true,greater_than: 300, less_than: 9999999}
   
-  has_many :images
-  has_many :items_categories
-  has_many :categories, through: :items_categories
+  has_many_attached :images
   has_one :shipment
   belongs_to :user
   belongs_to :brand
-  belongs_to :category
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
   accepts_nested_attributes_for :shipment
   accepts_nested_attributes_for :brand
+  has_many :items_categories
+  has_many :categories, through: :items_categories
+  accepts_nested_attributes_for :items_categories, allow_destroy: true
 
   enum size: {"--": 0, "XXS以下": 1, "XS(SS)": 2, "S": 3, "M": 4,"L": 5,"XL(LL)": 6, "2XL(3L)": 7, "3XL(4L)": 8, "4XL(5L)以上": 9,"FREESIZE": 10}, _suffix: true
 
