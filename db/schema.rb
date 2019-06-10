ecdf3ce865f2089830bdf5913feb8849e9ccb8b2
sino <smk16sound@gmail.com>
2019-06-10 17:39:21 +0900

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_072214) do
+ActiveRecord::Schema.define(version: 2019_06_10_082655) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_072214) do
     t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "shipment_id"
     t.index ["name"], name: "index_items_on_name"
   end
 
