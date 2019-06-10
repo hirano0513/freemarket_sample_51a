df6f6a0055271565700e35b8c0bbdf56c88f3bbd
sino <smk16sound@gmail.com>
2019-06-10 17:39:35 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,6 @@ class ItemsController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @item = Item.find(params[:id])
-    @shipment = Shipment.find_by(item_id: params[:id])
   end
 
 end
