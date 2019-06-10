df3f81f510d55221acaff64f3def2f2ab6e3f520
hirano0513 <shimaimo@gmail.com>
2019-06-10 15:25:21 +0900

    Update index.html.haml & items_controller.rb
    @brand1_name = @picup_brand1[0].brand.name
    を
    @brand1 = @picup_brand1[0].brand
    に変更(後でnameとidを使用するため)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
 
     # ブランド新着1
     @picup_brand1 = Brand.find(1).items.order("created_at ASC").limit(4)
-    @brand1_name = @picup_brand1[0].brand.name
+    @brand1 = @picup_brand1[0].brand
 
     # 全アイテムからデータ取り出し(仮)
     @items = Item.order("created_at DESC").limit(4)
