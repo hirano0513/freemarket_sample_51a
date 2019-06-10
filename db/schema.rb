061ca07deadff403446641d32af4019f1f70044f
YamamotoMao35 <nokiroram@gmail.com>
2019-06-10 23:20:43 +0900

    Update shipment_id column
    Update shipment_id column
    
    shipment_idにforeign_keyを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_092548) do
+ActiveRecord::Schema.define(version: 2019_06_10_141814) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_06_10_092548) do
     t.text "description", null: false
     t.integer "shipment_id"
     t.index ["name"], name: "index_items_on_name"
+    t.index ["shipment_id"], name: "fk_rails_1b5c4f46ae"
   end
 
   create_table "items_categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -134,6 +135,7 @@ ActiveRecord::Schema.define(version: 2019_06_10_092548) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "credit_cards", "users"
+  add_foreign_key "items", "shipments"
   add_foreign_key "personals", "users"
   add_foreign_key "sns_credentials", "users"
 end
