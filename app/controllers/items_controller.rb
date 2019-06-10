7f6c62958d247296f7aeaf1da6b2151192b41f1c
sino <smk16sound@gmail.com>
2019-06-10 15:32:18 +0900

    showメソッド定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,15 @@
 class ItemsController < ApplicationController
+  
   def index
   end
 
   def new
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @item = Item.find(params[:id])
+    @shipment = Shipment.find_by(item_id: params[:id])
+  end
+
 end
