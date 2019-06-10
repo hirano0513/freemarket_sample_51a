81a81637fde7de0d147ff25dbc8e67e15aa865bf
sino <smk16sound@gmail.com>
2019-06-10 15:31:38 +0900

    enum追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,6 @@ class Item < ApplicationRecord
   belongs_to :brand
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
+
+  enum item_status: [['---',""], '新品、未使用', '未使用に近い', '目立った傷や汚れなし', 'やや傷や汚れあり', '傷や汚れあり', '全体的に状態が悪い']
 end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,4 +2,8 @@ class Shipment < ApplicationRecord
   belongs_to :item
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  enum cost_payer: [['---',""], '送料込み(出品者負担)', '着払い(購入者負担)']
+  enum method: ['未定', 'らくらくメルカリ便', 'ゆうメール', 'レターパック', '普通郵便(定形、定形外)', 'クロネコヤマト', 'ゆうパック', 'クリックポスト', 'ゆうパケット']
+  enum days: ['1〜2日で発送', '2〜3日で発送', '4〜7日で発送']
 end
