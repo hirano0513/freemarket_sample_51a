d1929d6ce19301cb0f3626ebefdccee16d631943
keita.nakagawa <saku.nk@icloud.com>
2019-06-11 16:57:34 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_06_10_092548) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -33,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_06_10_092548) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -132,7 +130,6 @@ ActiveRecord::Schema.define(version: 2019_06_10_092548) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "credit_cards", "users"
   add_foreign_key "personals", "users"
   add_foreign_key "sns_credentials", "users"
 end
