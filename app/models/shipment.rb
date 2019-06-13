eaacd1b7dbddb0d9b86672e5bfc0fd8d849f6fcc
YamamotoMao35 <nokiroram@gmail.com>
2019-06-13 11:58:41 +0900

    Update shipment.rb

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -3,10 +3,10 @@ class Shipment < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  enum cost_payer: {"送料込み(出品者負担)": 1, "着払い(購入者負担)": 2}, _suffix: true
+  enum cost_payer: {postage_included: 1, cash_on_delivery: 2}
 
-  enum method: {"未定": 1, "らくらくメルカリ便": 2,"ゆうメール": 3,"レターパック": 4,"普通郵便(定形、定型外)": 5, "クロネコヤマト": 6, "ゆうパック": 7, "クリックポケット": 8, "ゆうパケット": 9}, _suffix: true
+  enum method: {undecided: 1, easy_courier_service: 2, postal_mail: 3, letter_pack: 4, regular_mail: 5, Kuroneko_Yamato: 6, postal_pack: 7, Click_Pocket: 8, postal_packet: 9}
 
-  enum days: {"1〜2日で発送": 1, "2〜3日で発送": 2, "4〜7日で発送": 3}, _suffix: true
+  enum days: {one_to_two_days: 1, two_to_three_days: 2, four_to_seven_days: 3}
 
 end
