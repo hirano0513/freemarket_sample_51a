fc8ccd59ad32767dbba4ae7a57f17116f1b53734
keita.nakagawa <saku.nk@icloud.com>
2019-06-14 17:05:14 +0900

    プロフィール変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(profile_params)
+    redirect_to controller: :users, action: :edit
   end
+
+  private
+  def profile_params
+    params.require(:user).permit(:nickname, :profile)
+  end
+
 end
