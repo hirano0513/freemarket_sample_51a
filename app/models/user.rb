1e951e012e1df6441d995c3565f2583418357025
YamamotoMao35 <nokiroram@gmail.com>
2019-06-17 16:46:44 +0900

    Update user.rb
    Update user.rb
    
    personalテーブルに登録できるように編集した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   has_many :sns_credentials, dependent: :destroy
+  has_one :personal, dependent: :destroy
+  accepts_nested_attributes_for :personal
   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :selling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
