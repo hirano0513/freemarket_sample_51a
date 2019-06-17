1a14938f4ebaf325838b9b5010e5942500e369fa
YamamotoMao35 <nokiroram@gmail.com>
2019-06-17 17:43:49 +0900

    Update show.html.haml
    Update show.html.haml
    
    本人確認画面に登録した名前とカナの名前が出るようにした。

--- app/controllers/personals_controller.rb
+++ app/controllers/personals_controller.rb
@@ -1,5 +1,11 @@
 class PersonalsController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def show
-    @personals = "name"
+    @personals = @user.personal
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
