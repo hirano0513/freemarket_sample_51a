a92d511b2b0a2425e97da02202f722bdc42ca279
YamamotoMao35 <nokiroram@gmail.com>
2019-06-20 14:11:13 +0900

    Update katakana_validation.js

--- app/assets/javascripts/katakana_validation.js
+++ app/assets/javascripts/katakana_validation.js
@@ -2,8 +2,6 @@ $(function(){
   $("#registration-submit").click(function(){
     var kana_first_name = $("input[name='user[personal_attributes][kana_first_name]']").val()
     var kana_last_name = $("input[name='user[personal_attributes][kana_last_name]']").val()
-    console.log(kana_first_name)
-    console.log(kana_last_name)
     if(kana_first_name == "" || kana_last_name == ""){
       alert("必須項目を入力して下さい。");
     } else {
