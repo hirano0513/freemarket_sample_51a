771b1ab0f71bad8d6c53af1ff501d9e1478cbf25
YamamotoMao35 <nokiroram@gmail.com>
2019-06-20 14:03:36 +0900

    Update kana_validation.js
    Update kana_validation.js
    
    フォームに入力されているときにアラートが出るように修正した。

--- app/assets/javascripts/kana_validation.js
+++ app/assets/javascripts/kana_validation.js
@@ -1,13 +1,21 @@
 $(function(){
   $("#registration-submit").click(function(){
-    if(!$("input[name='user[personal_attributes][kana_first_name]']").val().match(/^[ァ-ロワヲンー \r\n\t]*$/)){
-      alert('カナ入力欄にはカタカナを入力してください。')
-    }
-    else if(!$("input[name='user[personal_attributes][kana_last_name]']").val().match(/^[ァ-ロワヲンー \r\n\t]*$/)){
-      alert('カナ入力欄にはカタカナを入力してください。')
-    }
-    else{
-      alert('登録できました。')
+    var kana_first_name = $("input[name='user[personal_attributes][kana_first_name]']").val()
+    var kana_last_name = $("input[name='user[personal_attributes][kana_last_name]']").val()
+    console.log(kana_first_name)
+    console.log(kana_last_name)
+    if(kana_first_name == "" || kana_last_name == ""){
+      alert("必須項目を入力して下さい。");
+    } else {
+      if(!kana_first_name.match(/^[ァ-ロワヲンー \r\n\t]*$/)){
+        alert('姓カナ入力欄にはカタカナを入力してください。')
+      }
+      else if(!kana_last_name.match(/^[ァ-ロワヲンー \r\n\t]*$/)){
+        alert('名カナ入力欄にはカタカナを入力してください。')
+      }
+      else{
+        alert('登録できました。')
+      }
     }
   })
 })
